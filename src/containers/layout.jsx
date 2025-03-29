76e7898130783f8f145813e5ae68e20433d2f694
Shivam <shivamkrsingh1911@gmail.com>
2025-03-29 16:28:36 +0530

    layout added

--- src/containers/layout.jsx
+++ src/containers/layout.jsx
@@ -1,1 +1,49 @@
-const hello = 'vkdfi';
+import React, { useState } from "react"
+import QueryEditor from "../components/codeEditor/index"
+import QuerySelector from "../components/dropDown/DropDown"
+import ResultsTable from "../components/table/ReactTable"
+import VerticalSplitter from "./splitter/verticalSplitter/VerticalSplitter"
+function Layout({ options, setOptions }) {
+  const [query, setQuery] = useState("")
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const butonList = [
+    {
+      label: "Run Query",
+      onClick: () => {
+        console.log("Running query:", query)
+        // Add logic to run the query and fetch results
+      },
+    },
+    {
+      label: "Save Query",
+      onClick: () => {
+        if (!query) {
+          alert("Please enter a query before saving.")
+          return
+        }
+        const key = prompt("Enter a name for the query:")
+
+        if (!key) {
+          alert("Query name cannot be empty.")
+          return
+        }
+
+        console.log("Saving query:", key, query)
+        // Add logic to save the query
+      },
+    },
+  ]
+  return (
+    <div className="layout" id="layout">
+      <div className="left">
+        <QuerySelector setQuery={setQuery} />
+      </div>
+      <VerticalSplitter>
+        <QueryEditor query={query} setQuery={setQuery} buttonList={butonList} />
+        <ResultsTable />
+      </VerticalSplitter>
+    </div>
+  )
+}
+
+export default Layout
